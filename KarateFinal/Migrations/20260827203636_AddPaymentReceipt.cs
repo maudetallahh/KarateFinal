afba6d019123fa0fe37e8858a96325cc763ac9d9
maudetallahh <m.audetallahh@example.com>
2026-08-28 14:12:38 +0300

    fix PaymentReceipt migration to use IF NOT EXISTS

--- KarateFinal/Migrations/20260827203636_AddPaymentReceipt.cs
+++ KarateFinal/Migrations/20260827203636_AddPaymentReceipt.cs
@@ -10,7 +10,19 @@ namespace KarateFinal.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql(@"
+        CREATE TABLE IF NOT EXISTS ""PaymentReceipts"" (
+            ""Id"" SERIAL PRIMARY KEY,
+            ""PlayerId"" INT NOT NULL,
+            ""ClubId"" INT NOT NULL,
+            ""Year"" INT NOT NULL,
+            ""Month"" INT NOT NULL,
+            ""Amount"" DECIMAL NOT NULL,
+            ""PaidDate"" TIMESTAMP NOT NULL,
+            ""Notes"" TEXT,
+            ""CreatedBy"" TEXT NOT NULL DEFAULT ''
+        )
+    ");
         }
 
         /// <inheritdoc />
