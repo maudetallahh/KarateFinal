1c48735070e2e380a8deeacf1f9249ed581ce925
maudetallahh <m.audetallahh@example.com>
2026-09-04 00:50:11 +0300

    add dark mode CSS to all pages

--- KarateFinal/obj/Debug/net8.0/KarateFinal.AssemblyInfo.cs
+++ KarateFinal/obj/Debug/net8.0/KarateFinal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KarateFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e8477a95711efc9d05895ae40899803d4146c2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6ccb226e61c8e66fe2042969c7b6054b51db503")]
 [assembly: System.Reflection.AssemblyProductAttribute("KarateFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KarateFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
