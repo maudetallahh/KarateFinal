c4f867444bcad5cadb89ee40f6657ab8c59b4efc
maudetallahh <m.audetallahh@example.com>
2026-09-12 21:48:27 +0300

    add player/official/logo images, QR code, national team page, tournament draw, clubs ranking, SEO

--- KarateFinal/obj/Debug/net8.0/KarateFinal.AssemblyInfo.cs
+++ KarateFinal/obj/Debug/net8.0/KarateFinal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KarateFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0054f09bc62f8f2917e4361d1f257e39709f526")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f992bf3ecce1982eba97ba07f1de6dd82d434d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("KarateFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KarateFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
